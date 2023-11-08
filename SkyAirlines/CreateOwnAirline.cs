9c563c561b559dc577b47b8982250be878242e4f
TomPro <Tomas.Prochazka2@student.copsu.cz>
2023-11-08 15:26:28 +0100

    CreateOwnAirline.cs - Design

---
+++ SkyAirlines/CreateOwnAirline.cs
@@ -0,0 +1,49 @@
+﻿using GMap.NET.MapProviders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SkyAirlines
+{
+    public partial class CreateOwnAirline : UserControl
+    {
+        public CreateOwnAirline()
+        {
+            InitializeComponent();
+            InitializeMap();
+        }
+
+        private void InitializeMap()
+        {
+            gMapControl.MapProvider = GMapProviders.GoogleMap;
+            gMapControl.ShowCenter = false;
+            gMapControl.MinZoom = 0;
+            gMapControl.MaxZoom = 24;
+            gMapControl.Zoom = 4;
+
+            Controls.Add(gMapControl);
+        }
+
+        private void btn_MouseEnter(object sender, EventArgs e)
+        {
+            Button btn;
+            btn = (Button)sender;
+            btn.FlatAppearance.BorderSize = 3;
+            btn.FlatAppearance.BorderColor = Color.RoyalBlue;
+        }
+
+        private void btn_MouseLeave(object sender, EventArgs e)
+        {
+            Button btn;
+            btn = (Button)sender;
+            btn.FlatAppearance.BorderSize = 0;
+            btn.FlatAppearance.BorderColor = Color.FromArgb(16, 47, 82);
+        }
+    }
+}
