a15ea999cc72625ed17e4dcfaef6fea74cb372ec
TomPro <Tomas.Prochazka2@student.copsu.cz>
2023-11-08 16:00:48 +0100

    👍

--- SkyAirlines/UserControls/FlightTracking.Designer.cs
+++ SkyAirlines/UserControls/FlightTracking.Designer.cs
@@ -434,7 +434,7 @@
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label9.ForeColor = System.Drawing.Color.Black;
-            this.label9.Location = new System.Drawing.Point(808, 10);
+            this.label9.Location = new System.Drawing.Point(817, 10);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(126, 20);
             this.label9.TabIndex = 6;
